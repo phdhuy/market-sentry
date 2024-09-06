b0643190457f92a62d9b6fc646e90d50c41e64a8
phduchuy <phanhoangduchuyy@gmail.com>
2024-09-07 00:03:22 +0700

    feat: scraping price stock nearly realtime

--- infrastructure/src/main/java/com/phdhuy/stock_alert/external/adapter/PriceStockVNAdapter.java
+++ infrastructure/src/main/java/com/phdhuy/stock_alert/external/adapter/PriceStockVNAdapter.java
@@ -2,14 +2,13 @@ package com.phdhuy.stock_alert.external.adapter;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.phdhuy.stock_alert.databases.influxdb.adapter.CreatePriceAssetAdapter;
 import com.phdhuy.stock_alert.external.constant.ExternalAPIConstant;
+import com.phdhuy.stock_alert.ports.outbound.messagebroker.RabbitMQPort;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import com.phdhuy.stock_alert.handler.PriceWebSocketHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
@@ -17,7 +16,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
@@ -28,29 +28,36 @@ public class PriceStockVNAdapter extends TextWebSocketHandler {
 
   private final WebDriver webDriver;
 
-  private final PriceWebSocketHandler priceWebSocketHandler;
+  private final RabbitMQPort rabbitMQPort;
 
-  private final CreatePriceAssetAdapter createPriceAssetAdapter;
-
-  @Scheduled(cron = "*/10 * * * * MON-FRI")
+  @EventListener(ApplicationReadyEvent.class)
   public void getStockPrice() throws JsonProcessingException {
     webDriver.get(ExternalAPIConstant.PRICE_STOCK);
 
     WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+//    WebElement tradingStatus =
+//        wait.until(
+//            ExpectedConditions.visibilityOfElementLocated(
+//                By.xpath(
+//                    "//div[contains(@class, 'chart-footer-vn30')]//div[contains(text(), 'Đóng cửa')]")));
+//    log.info(tradingStatus.getText());
+    while (true) {
+      List<Map<String, String>> priceList = new ArrayList<>();
+      List<WebElement> priceElements =
+          wait.until(
+              ExpectedConditions.presenceOfAllElementsLocatedBy(
+                  By.xpath("//*[contains(@id, '_lastPrice_value')]")));
 
-    List<WebElement> priceElements =
-        wait.until(
-            ExpectedConditions.presenceOfAllElementsLocatedBy(
-                By.xpath("//*[contains(@id, '_lastPrice_value')]")));
-
-    for (WebElement stock : priceElements) {
-      String id = stock.getAttribute("id").substring(0, 3);
-      String price = stock.getText();
-      Map<String, String> map = new HashMap<>();
-      map.put(id, price);
-      String jsonString = new ObjectMapper().writeValueAsString(map);
-      priceWebSocketHandler.handleTextMessage(jsonString);
-      createPriceAssetAdapter.createPriceAssetPort(id, id, Double.parseDouble(price));
+      for (WebElement stock : priceElements) {
+        String id = stock.getAttribute("id").substring(0, 3);
+        String price = stock.getText();
+        Map<String, String> map = new HashMap<>();
+        map.put(id, price);
+        priceList.add(map);
+      }
+      ObjectMapper objectMapper = new ObjectMapper();
+      String jsonString = objectMapper.writeValueAsString(priceList);
+      rabbitMQPort.sendMessage(jsonString);
     }
   }
 }
