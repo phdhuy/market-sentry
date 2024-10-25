491fa4f39a0d91d4692ff2f05f03678040e56bf8
phduchuy <phanhoangduchuyy@gmail.com>
2024-10-25 22:06:27 +0700

    config: config selenium firefox

--- infrastructure/src/main/java/com/phdhuy/stock_alert/infrastructure/databases/postgresql/repository/PortfolioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.phdhuy.stock_alert.infrastructure.databases.postgresql.repository;
-
-import com.phdhuy.stock_alert.infrastructure.databases.postgresql.entity.PortfolioEntity;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PortfolioRepository extends JpaRepository<PortfolioEntity, UUID> {}
