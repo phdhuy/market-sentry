8ae173e5d2f07bf17aa4490c324a68f271c26cd0
phduchuy <phanhoangduchuyy@gmail.com>
2025-02-12 23:19:42 +0700

    update: improve perf of apache flink

--- infrastructure/src/main/java/com/phdhuy/stock_alert/infrastructure/external/flink/datasource/AlertDatabaseSource.java
+++ infrastructure/src/main/java/com/phdhuy/stock_alert/infrastructure/external/flink/datasource/AlertDatabaseSource.java
@@ -2,33 +2,42 @@ package com.phdhuy.stock_alert.infrastructure.external.flink.datasource;
 
 import com.phdhuy.stock_alert.domain.alert.model.Alert;
 import com.phdhuy.stock_alert.domain.alert.port.outbound.AlertRepositoryPort;
-import lombok.RequiredArgsConstructor;
+import com.phdhuy.stock_alert.shared.utils.SpringContext;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
+@Slf4j
 @Component
-@RequiredArgsConstructor
-public class AlertDatabaseSource extends RichSourceFunction<List<Alert>> {
+public class AlertDatabaseSource extends RichSourceFunction<Alert> {
 
-    private final AlertRepositoryPort alertRepositoryPort;
+  private transient AlertRepositoryPort alertRepositoryPort;
+  private volatile boolean running = true;
 
-    private volatile boolean running = true;
+  @Override
+  public void open(Configuration parameters) {
+    this.alertRepositoryPort = SpringContext.getBean(AlertRepositoryPort.class);
+  }
 
+  @Override
+  public void run(SourceContext<Alert> ctx) throws Exception {
+    while (running) {
+      List<Alert> activeAlerts = alertRepositoryPort.getListAlertActive();
+      log.info("Broadcasting {} active alerts", activeAlerts.size());
 
-    @Override
-    public void run(SourceContext<List<Alert>> ctx) throws Exception {
-        while (running) {
-            List<Alert> alerts = alertRepositoryPort.getListAlertActive();
-            ctx.collect(alerts);
-            Thread.sleep(30000);
-        }
+      for (Alert alert : activeAlerts) {
+        ctx.collect(alert);
+      }
+
+      Thread.sleep(30000);
     }
+  }
 
-    @Override
-    public void cancel() {
-        running = false;
-    }
+  @Override
+  public void cancel() {
+    running = false;
+  }
 }
 
